33d562f39ece6a1eace677dbd5fb22aeab6ce008
kristianwiklund <githubkristian@snabela.nl>
2016-10-31 13:57:30 +0100

    file rename

--- frontend/program.py
+++ frontend/program.py
@@ -12,3 +12,45 @@
 # beep sound the alarm
 # requester <text> show a requester with text and wait for the user to click OK
 
+class Program:
+
+	oldstep=255
+	xdata = {}
+	ydata = {}
+	xdata["HLT"] = []
+	ydata["HLT"] = []
+	xdata["MLT"] = []
+	ydata["MLT"] = []
+
+
+	def __init__(self, w, bt,stepWidgets,nextwidget,stopalarmwidget,plot):
+	  self.stepWidgets = stepWidgets
+	  nextwidget.clicked.connect(self.nextstep)
+	  stopalarmwidget.clicked.connect(self.stopalarm)
+	  self.plot = plot
+	  self.bt = bt
+
+	def stopalarm(self):
+	  # code to change the alarm indicator back to inactive
+	  self.bt.stopAlarm()
+
+	def nextstep(self):
+	  self.bt.advStep()
+
+	def update(self):
+	
+		# need to update the progress bars and display which step is active
+		# change this
+		fullstatus = self.bt.getFullStatus()
+		#               print ("step" + str(brewstep))
+
+
+		self.xdata["HLT"].append(fullstatus["timestamp"])
+		self.ydata["HLT"].append(float(fullstatus["HLT"]["temp"]))
+
+		self.xdata["MLT"].append(fullstatus["timestamp"])
+		self.ydata["MLT"].append(float(fullstatus["MLT"]["temp"]))
+		#print self.ydata
+
+		self.plot.update_plot(self.xdata, self.ydata)
+
